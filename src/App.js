ba9b11d2f57d8f3ab3fe54e286b0dff35c90aa79
Yassine Hakim <47417597+YassineHk@users.noreply.github.com>
2020-01-11 23:59:14 +0100

    fix
    fix package.json problem

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
-import Cart from './Components/Card'
-import CartData from './Data'
-import {CSSTransition}  from 'react-transition-group'
-
+import React, { Component } from "react";
+import "./App.css";
+import Cart from "./Components/Card";
+import CartData from "./Data";
+import { CSSTransition } from "react-transition-group";
 
 export default class App extends Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
       Data: CartData,
       val: 0,
-    }
+      rep:""
+    };
   }
- 
+
   handleChange() {
     if (this.state.val < 4) {
       this.setState({
@@ -25,19 +24,21 @@ export default class App extends Component {
 
   render() {
     return (
-      <div >
+      <div>
         <header className="App-header">
-          <CSSTransition
-            appear={true}
-            timeout={20000}
-          >
+          <CSSTransition appear={true} timeout={20000}>
             <Cart ask={this.state.Data[this.state.val]} />
           </CSSTransition>
-          <div >
-            <input className="btn" type="submit" value="Suivant" onClick={this.handleChange.bind(this)} />
+          <div>
+            <input
+              className="btn"
+              type="submit"
+              value="Suivant"
+              onClick={this.handleChange.bind(this)}
+            />
           </div>
         </header>
-      </div >
+      </div>
     );
   }
 }
